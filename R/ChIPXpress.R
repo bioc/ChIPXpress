ec6e695f5d25104e1fa98970bf0e636b1200a5d0
George Wu <gewu@jhsph.edu>
2012-08-30 21:07:14 +0000

    Modified the ChIPXpress algorithm to include w,c parameters and related help files
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ChIPXpress@68973 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ChIPXpress.R
+++ R/ChIPXpress.R
@@ -1,12 +1,12 @@
 ChIPXpress <-
-function(TFID,ChIP,DB){
+function(TFID,ChIP,DB,w=0.1,c=0){
   if(sum(TFID %in% rownames(DB))==0) {
     stop("ERROR: TF EntrezID cannot be found in database")
   } else {
-    index <- DB[as.character(TFID),] > 0
+    index <- DB[as.character(TFID),] > c
     if(sum(index) < 2) {
         stop("ERROR: Need more samples in compendium! 
-Need at least 2 samples in which TF is above the mean TF expression")
+Need at least 2 samples in which TF is above TF expression cutoff")
     }
     TFmat <- DB[as.character(TFID),index]
     DBmat <- DB[,index]
@@ -15,11 +15,21 @@ Need at least 2 samples in which TF is above the mean TF expression")
     Out <- sort(Out,decreasing=T)
     Miss.Genes <- ChIP[!(ChIP %in% rownames(DB))]
 
+    ## Weight Ranking Combination
+    x <- unique(ChIP); y <- unique(names(Out))
+    xc <- x[x %in% y]; yc <- y[y %in% x]
+    Score <- rep(0, length(xc))
+    for(i in 1:length(xc)){
+        Score[i] <- w*i+(1-w)*which(yc == xc[i])
+    }
+    names(Score) <- xc
+    Score <- sort(Score)
+
     message("Success!")
-    message(length(Out)," out of ",length(ChIP)," (",
-            100*round(length(Out)/length(ChIP),3),"%) genes processed")
+    message(length(Score)," out of ",length(ChIP)," (",
+            100*round(length(Score)/length(ChIP),3),"%) genes processed")
     
-    return(list(ChIPXpress.ranking = Out,
+    return(list(ChIPXpress.ranking = Score,
                 MissingEntrezGeneIDs = Miss.Genes))
   }
 }
