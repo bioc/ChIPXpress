3aa4136d1b0db964db833cdafbe28203b0e982f8
metygl <33673930+metygl@users.noreply.github.com>
2017-12-22 11:09:29 -0800

    UnitTests setup again

--- inst/unitTests/test_all.R
+++ inst/unitTests/test_all.R
@@ -1,17 +1,17 @@
 test_all <- function()
 {
-    #GSM_ids <- c("GSM24056","GSM24058","GSM24060",
-    #             "GSM94856","GSM94857","GSM94858")
-    #t <- tempdir()
-    #DB <- buildDatabase(GSMfiles=GSM_ids,SaveDir=t)
-    #library(mouse4302.db)
-    #EntrezID <- mget(as.character(rownames(DB)),mouse4302ENTREZID)
-    #rownames(DB) <- as.character(EntrezID)
-    #cleanDB <- cleanDatabase(DB,SaveFile="newDB_GPL1261.bigmemory",
-    #                         SavePath=t)
-    #data(Oct4ESC_ChIPgenes)
-    #out <- ChIPXpress(TFID="18999",ChIP=Oct4ESC_ChIPgenes$EntrezID,DB=cleanDB)
-    #checkEquals(class(out), "list")
-    #checkEquals(length(out), 2)
+    GSM_ids <- c("GSM24056","GSM24058","GSM24060",
+                 "GSM94856","GSM94857","GSM94858")
+    t <- tempdir()
+    DB <- buildDatabase(GSMfiles=GSM_ids,SaveDir=t)
+    library(mouse4302.db)
+    EntrezID <- mget(as.character(rownames(DB)),mouse4302ENTREZID)
+    rownames(DB) <- as.character(EntrezID)
+    cleanDB <- cleanDatabase(DB,SaveFile="newDB_GPL1261.bigmemory",
+                             SavePath=t)
+    data(Oct4ESC_ChIPgenes)
+    out <- ChIPXpress(TFID="18999",ChIP=Oct4ESC_ChIPgenes$EntrezID,DB=cleanDB)
+    checkEquals(class(out), "list")
+    checkEquals(length(out), 2)
     
 }
